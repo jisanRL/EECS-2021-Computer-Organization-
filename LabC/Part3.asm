bc11d23ce567d4d9034dd52d5adfc78049418b09
Jisan <jisanrezalatif@gmail.com>
2018-10-02 19:46:27 -0400

    Update Part3.asm

--- LabC/Part3.asm
+++ LabC/Part3.asm
@@ -7,12 +7,12 @@
 	ecall	x6, x3, 5		//get input for A 
 	ecall	x7, x4, 5		//get input for B
 	ecall	x8, x5, 5		//get input for C 
-	bge	x7, x6, ELSE		// if !(A>B), goes to ELSE block
+	bge	x7, x6, ELSE		//if !(A>B), goes to ELSE block
 	addi	x9, x0, 5		//set x9 to 5
 	bne	x8, x9, ELSE		//if !(c==5), go to ELSE block 
 	addi	x1, x0, 1		//execute Y=1, put result in x1 
 	addi	x2, x0, EXIT 		//execute Z=2, put result in x2 
-ELSE:   addi	   x1, x0, 0			//label ELSE; execute Y=0, put result in x1
+ELSE:   addi	   x1, x0, 0		//label ELSE; execute Y=0, put result in x1
 	addi 	x2, x0, 0		//execute z=0, put result in x2  
 EXIT: 
 
